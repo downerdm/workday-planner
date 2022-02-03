eafbec7fd2769d3dc21b06a3302a4f932121d41a
Dennis Downer <downerdm@gmail.com>
2022-02-02 21:18:37 -0500

    Modified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,55 @@ var today = moment();
 $("#currentDay").text(today.format('dddd MMMM Do YYYY'));
 console.log(today);
 
-var firstDayBlock= $("#9").find("textarea");
-firstDayBlock.val(localStorage.getItem("block01"));
+var block09= $("#09").find("textarea");
+block09.val(localStorage.getItem("block09"));
 var firstBlock=$("#button09");
 
+var block09= $("#10").find("textarea");
+block09.val(localStorage.getItem("block10"));
+var firstBlock=$("#button10");
+
+var block09= $("#11").find("textarea");
+block09.val(localStorage.getItem("block11"));
+var firstBlock=$("#button11");
+
+var block09= $("#12").find("textarea");
+block09.val(localStorage.getItem("block12"));
+var firstBlock=$("#button12");
+
+var block09= $("#13").find("textarea");
+block09.val(localStorage.getItem("block13"));
+var firstBlock=$("#button13");
+
+var block09= $("#14").find("textarea");
+block09.val(localStorage.getItem("block14"));
+var firstBlock=$("#button14");
+
+var block09= $("#15").find("textarea");
+block09.val(localStorage.getItem("block15"));
+var firstBlock=$("#button15");
+
+var block09= $("#16").find("textarea");
+block09.val(localStorage.getItem("block16"));
+var firstBlock=$("#button16");
+
+var block09= $("#17").find("textarea");
+block09.val(localStorage.getItem("block17"));
+var firstBlock=$("#button17");
+
 firstBlock.on("click", function(){
-    localStorage.setItem("block01", firstDayBlock.val());
+    localStorage.setItem("block09", block09.val());
 })
 
-console.log(localStorage.getItem("block01"));
+console.log(localStorage.getItem("block09"));
+console.log(localStorage.getItem("block10"));
+console.log(localStorage.getItem("block11"));
+console.log(localStorage.getItem("block12"));
+console.log(localStorage.getItem("block13"));
+console.log(localStorage.getItem("block14"));
+console.log(localStorage.getItem("block15"));
+console.log(localStorage.getItem("block16"));
+console.log(localStorage.getItem("block17"));
 
 console.log($(".container")[0]);
 
