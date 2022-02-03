971c13b5c8cc15e36cb70ba74cf2cf4437ce51ff
Dennis Downer <downerdm@gmail.com>
2022-02-03 01:27:32 -0500

    Coding changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,85 @@
+// Banner date
 var today = moment();
 $("#currentDay").text(today.format('dddd MMMM Do YYYY'));
 console.log(today);
 
+// Blocks for local storage
 var block09= $("#09").find("textarea");
 block09.val(localStorage.getItem("block09"));
 var firstBlock=$("#button09");
 
-var block09= $("#10").find("textarea");
-block09.val(localStorage.getItem("block10"));
-var firstBlock=$("#button10");
+var block10= $("#10").find("textarea");
+block10.val(localStorage.getItem("block10"));
+var secondBlock=$("#button10");
 
-var block09= $("#11").find("textarea");
-block09.val(localStorage.getItem("block11"));
-var firstBlock=$("#button11");
+var block11= $("#11").find("textarea");
+block11.val(localStorage.getItem("block11"));
+var thirdBlock=$("#button11");
 
-var block09= $("#12").find("textarea");
-block09.val(localStorage.getItem("block12"));
-var firstBlock=$("#button12");
+var block12= $("#12").find("textarea");
+block12.val(localStorage.getItem("block12"));
+var fourthBlock=$("#button12");
 
-var block09= $("#13").find("textarea");
-block09.val(localStorage.getItem("block13"));
-var firstBlock=$("#button13");
+var block13= $("#13").find("textarea");
+block13.val(localStorage.getItem("block13"));
+var fifthBlock=$("#button13");
 
-var block09= $("#14").find("textarea");
-block09.val(localStorage.getItem("block14"));
-var firstBlock=$("#button14");
+var block14= $("#14").find("textarea");
+block14.val(localStorage.getItem("block14"));
+var sixthBlock=$("#button14");
 
-var block09= $("#15").find("textarea");
-block09.val(localStorage.getItem("block15"));
-var firstBlock=$("#button15");
+var block15= $("#15").find("textarea");
+block15.val(localStorage.getItem("block15"));
+var seventhBlock=$("#button15");
 
-var block09= $("#16").find("textarea");
-block09.val(localStorage.getItem("block16"));
-var firstBlock=$("#button16");
+var block16= $("#16").find("textarea");
+block16.val(localStorage.getItem("block16"));
+var eighthBlock=$("#button16");
 
-var block09= $("#17").find("textarea");
-block09.val(localStorage.getItem("block17"));
-var firstBlock=$("#button17");
+var block17= $("#17").find("textarea");
+block17.val(localStorage.getItem("block17"));
+var ninthBlock=$("#button17");
 
+// Rewriting local storage back to textbox
 firstBlock.on("click", function(){
     localStorage.setItem("block09", block09.val());
 })
 
-console.log(localStorage.getItem("block09"));
-console.log(localStorage.getItem("block10"));
-console.log(localStorage.getItem("block11"));
-console.log(localStorage.getItem("block12"));
-console.log(localStorage.getItem("block13"));
-console.log(localStorage.getItem("block14"));
-console.log(localStorage.getItem("block15"));
-console.log(localStorage.getItem("block16"));
-console.log(localStorage.getItem("block17"));
+secondBlock.on("click", function(){
+    localStorage.setItem("block10", block10.val());
+})
 
-console.log($(".container")[0]);
+thirdBlock.on("click", function(){
+    localStorage.setItem("block11", block11.val());
+})
 
+fourthBlock.on("click", function(){
+    localStorage.setItem("block12", block12.val());
+})
+
+fifthBlock.on("click", function(){
+    localStorage.setItem("block13", block13.val());
+})
+
+sixthBlock.on("click", function(){
+    localStorage.setItem("block14", block14.val());
+})
+
+seventhBlock.on("click", function(){
+    localStorage.setItem("block15", block15.val());
+})
+
+eighthBlock.on("click", function(){
+    localStorage.setItem("block16", block16.val());
+})
+
+ninthBlock.on("click", function(){
+    localStorage.setItem("block17", block17.val());
+})
+
+// Changing the box color according to "past", "present", and "future" events based
+// on current time.
 $('.container').children('div').each(function () {
-    //$(this).find('div').text() = 9:00 a.m.
-    console.log(today.startOf('hour').fromNow());
-
-    console.log(today.format('hh:mm A'));
     let timeText = $(this).find('div').text();
     let timeMoment = moment(timeText, 'hh:mm A');
     if(timeMoment.isBefore(today)){
@@ -68,6 +89,4 @@ $('.container').children('div').each(function () {
     } else if(timeMoment.isAfter(today)){
         $(this).find('textarea').addClass('future');
     }
-
-    console.log($(this).find('div').text()); // "this" is the current element in the loop
 });
